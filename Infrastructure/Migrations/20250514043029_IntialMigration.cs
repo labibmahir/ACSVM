bd470607cb6d73ee5413aab10ccc0f5e00471858
etl <mahir.dev@excelbd.com>
2025-05-14 18:29:57 +0600

    Update Migrations

---
+++ Infrastructure/Migrations/20250514043029_IntialMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CellPhone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    CountryCode = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsAccountActive = table.Column<bool>(type: "bit", nullable: false),
+                    OrganizationId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<byte>(type: "tinyint", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.Oid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250514122415_InitialMigrationPerson&Device.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationPersonDevice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "UserAccounts",
+                type: "smalldatetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "UserAccounts",
+                type: "smalldatetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AccessLevels",
+                columns: table => new
+                {
+                    Oid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessLevels", x => x.Oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PersonNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Gender = table.Column<byte>(type: "tinyint", nullable: false),
+                    IsDeviceAdministrator = table.Column<bool>(type: "bit", nullable: false),
+                    PersonType = table.Column<byte>(type: "tinyint", nullable: false),
+                    ValidateStartPeriod = table.Column<DateTime>(type: "smalldatetime", nullable: false),
+                    ValidateEndPeriod = table.Column<DateTime>(type: "smalldatetime", nullable: false),
+                    UserVerifyMode = table.Column<byte>(type: "tinyint", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Oid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModelName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MacAddress = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    FirmwareVersion = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    FirmwareReleasedDate = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    SerialNumber = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    DeviceIP = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Port = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    DeviceLicence = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    AccessLevelId = table.Column<int>(type: "int", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_Devices_AccessLevels_AccessLevelId",
+                        column: x => x.AccessLevelId,
+                        principalTable: "AccessLevels",
+                        principalColumn: "Oid");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FingerPrints",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FingerNumber = table.Column<byte>(type: "tinyint", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FingerPrints", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_FingerPrints_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentifiedAssignCards",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CardId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsPermanent = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false),
+                    ExpireDate = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentifiedAssignCards", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_IdentifiedAssignCards_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IdentifiedAssignCards_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PeopleImages",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageBase64 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageData = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PeopleImages", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_PeopleImages_Persons_Oid",
+                        column: x => x.Oid,
+                        principalTable: "Persons",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentifiedAssignDevices",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeviceId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentifiedAssignDevices", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_IdentifiedAssignDevices_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IdentifiedAssignDevices_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_AccessLevelId",
+                table: "Devices",
+                column: "AccessLevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FingerPrints_PersonId",
+                table: "FingerPrints",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentifiedAssignCards_CardId",
+                table: "IdentifiedAssignCards",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentifiedAssignCards_PersonId",
+                table: "IdentifiedAssignCards",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentifiedAssignDevices_DeviceId",
+                table: "IdentifiedAssignDevices",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentifiedAssignDevices_PersonId",
+                table: "IdentifiedAssignDevices",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FingerPrints");
+
+            migrationBuilder.DropTable(
+                name: "IdentifiedAssignCards");
+
+            migrationBuilder.DropTable(
+                name: "IdentifiedAssignDevices");
+
+            migrationBuilder.DropTable(
+                name: "PeopleImages");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "AccessLevels");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "UserAccounts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "smalldatetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "UserAccounts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "smalldatetime",
+                oldNullable: true);
+        }
+    }
+}
