be20dfc46aa44fccd6ce43ef3455a41ab8734ce5
etl <mahir.dev@excelbd.com>
2025-05-21 09:52:41 +0600

    update dev

---
+++ Infrastructure/Migrations/20250519123213_UpdateAppointment.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Slots_SlotId",
+                table: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Slots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_SlotId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "SlotId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "Appointments",
+                type: "time(7)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCancelled",
+                table: "Appointments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Appointments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                table: "Appointments",
+                type: "time(7)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "IsCancelled",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SlotId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Slots",
+                columns: table => new
+                {
+                    Oid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "smalldatetime", nullable: true),
+                    EndTime = table.Column<TimeSpan>(type: "time(7)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    OrganizationId = table.Column<int>(type: "int", nullable: true),
+                    StartTime = table.Column<TimeSpan>(type: "time(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slots", x => x.Oid);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_SlotId",
+                table: "Appointments",
+                column: "SlotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Slots_SlotId",
+                table: "Appointments",
+                column: "SlotId",
+                principalTable: "Slots",
+                principalColumn: "Oid",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
